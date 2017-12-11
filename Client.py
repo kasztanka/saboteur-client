343504c225d10076d13986a7d7d5b09d36dd69c3
kasztanka <m.lipowska@onet.pl>
2017-12-11 19:55:39 +0100

    Add thread that listens for server's responses

--- Client.py
+++ Client.py
@@ -1,33 +1,49 @@
 import socket
 import struct
+import threading
+from enum import Enum
+
+
+class Code(Enum):
+    MESSAGE = 1
 
 
 class Client():
-
     BUFFER_SIZE = 4
 
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        server_address = ('192.168.0.106', 42010)
+        server_address = ('192.168.0.47', 42010)
         print('connecting to {} port {}'.format(*server_address))
         self.sock.connect(server_address)
         try:
-            self.send_text('Ala budzi cara durne emu fika')
-            self.receive_text()
+            t1 = threading.Thread(target=self.listen)
+            t1.start()
+            text = 'Ala budzi cara'
+            self.send_message(text)
+            t1.join()
         finally:
             print('closing socket')
             self.sock.close()
 
+    def send_message(self, text):
+        self.send_int(Code.MESSAGE.value)
+        self.send_text(text)
+
     def send_int(self, num):
         packed_int = struct.pack('!i', num)
-        print('sending: ', num)
         self.sock.sendall(packed_int)
 
     def send_text(self, text):
         self.send_int(len(text))
-        print('sending: ', text)
         self.sock.sendall(text.encode())
+
+    def listen(self):
+        while(True):
+            message_num = self.receive_int()
+            if message_num == Code.MESSAGE.value:
+                print('Odebrano wiadomość: ' + self.receive_text())
 
     def receive_buffer(self, amount_expected):
         buffer = []
@@ -43,14 +59,12 @@ class Client():
     def receive_int(self):
         buffer = self.receive_buffer(4)
         number = struct.unpack('!i', b''.join(buffer))[0]
-        print('receiving: ', number)
         return number
 
     def receive_text(self):
         length = self.receive_int()
         buffer = self.receive_buffer(length)
         text = b''.join(buffer).decode('utf-8')
-        print('receiving: ', text)
         return text
 
 
